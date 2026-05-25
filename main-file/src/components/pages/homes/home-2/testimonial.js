f5b482e12f442d199be1c7b5f8d7ca195f84e4fe
Your Name <anujkumar@Anujs-Mac-mini.local>
2026-05-25 20:25:44 +0530

    Update testimonial brand logo imports to use the new black and white PNG images

--- main-file/src/components/pages/homes/home-2/testimonial.js
+++ main-file/src/components/pages/homes/home-2/testimonial.js
@@ -1,19 +1,19 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Autoplay, Navigation} from 'swiper/modules';
 import testimonialData from "../../../data/testimonial-data";
-import brand1 from "../../../../assets/img/brand-logo/acc.webp";
-import brand2 from "../../../../assets/img/brand-logo/asian paint.webp";
-import brand3 from "../../../../assets/img/brand-logo/burger.webp";
-import brand4 from "../../../../assets/img/brand-logo/dulux.webp";
-import brand5 from "../../../../assets/img/brand-logo/haveels.webp";
-import brand6 from "../../../../assets/img/brand-logo/magadh.webp";
-import brand7 from "../../../../assets/img/brand-logo/novuco.webp";
-import brand8 from "../../../../assets/img/brand-logo/pather.webp";
-import brand9 from "../../../../assets/img/brand-logo/rungta.webp";
-import brand10 from "../../../../assets/img/brand-logo/sail.webp";
-import brand11 from "../../../../assets/img/brand-logo/tata-parvesh.webp";
-import brand12 from "../../../../assets/img/brand-logo/tata-tiscon.webp";
-import brand13 from "../../../../assets/img/brand-logo/ultrateck.webp";
+import brand1 from "../../../../assets/img/brand-logo/1.png";
+import brand2 from "../../../../assets/img/brand-logo/2.png";
+import brand3 from "../../../../assets/img/brand-logo/3.png";
+import brand4 from "../../../../assets/img/brand-logo/4.png";
+import brand5 from "../../../../assets/img/brand-logo/5.png";
+import brand6 from "../../../../assets/img/brand-logo/6.png";
+import brand7 from "../../../../assets/img/brand-logo/7.png";
+import brand8 from "../../../../assets/img/brand-logo/8.png";
+import brand9 from "../../../../assets/img/brand-logo/9.png";
+import brand10 from "../../../../assets/img/brand-logo/10.png";
+import brand11 from "../../../../assets/img/brand-logo/11.png";
+import brand12 from "../../../../assets/img/brand-logo/12.png";
+import brand13 from "../../../../assets/img/brand-logo/13.png";
 
 const brandLogos = [
     brand1, brand2, brand3, brand4, brand5, brand6, brand7,
